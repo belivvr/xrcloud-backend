cefb4a7d5aac2156bff9c93f68da4760d2d28452
jdm <zizi27177@naver.com>
2023-08-18 07:13:25 +0000

    .

--- src/outdoor/outdoor.controller.ts
+++ src/outdoor/outdoor.controller.ts
@@ -7,29 +7,12 @@ export class OutdoorController {
     constructor(private readonly outdoorService: OutdoorService) {}
 
     @Post('event')
-    @HttpCode(201)
     async createEvent(@Body() createEventDto: CreateEventDto) {
-        return
+        return await this.outdoorService.createEvent(createEventDto)
     }
-
-    // @Post('event')
-    // async createEvent(@Body() createEventDto: CreateEventDto) {
-    //     return await this.outdoorService.createEvent(createEventDto)
-    // }
 
     @Get('option/:optionId')
     async getOption(@Param('optionId') optionId: string) {
-        const temp = {
-            token: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJyZXQiLCJleHAiOjE2OTk1MTcyODUsImlhdCI6MTY5MjI1OTY4NSwiaXNzIjoicmV0IiwianRpIjoiZjE4MmRhNjMtYzIwZC00NzIxLTgzMjAtNjk0MmRhZTgyMjA5IiwibmJmIjoxNjkyMjU5Njg0LCJzdWIiOiIxNTY0ODY2MjI0NzU0NTI0MTc2IiwidHlwIjoiYWNjZXNzIn0.3WWpeaQClBQWSgPrGDgphFObrLC4kxaNXJTI_yxKdXw3E5y9_qzi6I08TuUAv1Or5npG8gD3OjOOxKhbBZs7GA',
-            eventCallback: 'https%3A%2F%2Fvevv-test.vevv.io%3A3300%2Foutdoor%2Fevent',
-            extra: 'projectId:f83f4cf2-4b1e-4182-b3bd-de62af3c8c93'
-        }
-
-        return temp
+        return await this.outdoorService.getOption(optionId)
     }
-
-    // @Get('option/:optionId')
-    // async getOption(@Param('optionId') optionId: string) {
-    //     return await this.outdoorService.getOption(optionId)
-    // }
 }
