2f5d67320c87db1e405b8ae1efe6e4bfb3e77f4e
jdm <zizi27177@naver.com>
2023-10-27 07:06:22 +0000

    add db.sh

--- scripts/db.sh
+++ scripts/db.sh
@@ -2,26 +2,33 @@
 set -ex
 
 cd "$(dirname "$0")"
-THISDIR=$(pwd)
-cd ..
-. ./env.sh
-cd $THISDIR
+
+source ../.env
+
+# sudo apt-get install nfs-common
 
 docker rm -f postgres
 
-if [ "$1" = "prod" ]; then
+if [ "$ENV" = "prod" ]; then
     sudo mkdir -p "$DB_VOLUME_DIR"
 
-    #마운트 실행
     sudo mount -t nfs $DB_NAS_LOCATION $DB_VOLUME_DIR
 
-    #마운트 정보 유지 설정(fstab 설정)
     echo "$DB_NAS_LOCATION $DB_VOLUME_DIR nfs, defaults 0 0" | sudo tee -a /etc/fstab
 
-    docker run --log-opt max-size=10m --log-opt max-file=3 -d --restart=always -p 5432:5432 --name db -d -e POSTGRES_PASSWORD="$DB_PASSWORD" -v "$DB_VOLUME_DIR":/var/lib/postgresql/data postgres:11-bullseye || true
+    docker run --restart always --name postgres -d \
+        --log-opt max-size=10m \
+        --log-opt max-file=3 \
+        -e POSTGRES_DB=$TYPEORM_DATABASE \
+        -e POSTGRES_USER=$TYPEORM_USERNAME \
+        -e POSTGRES_PASSWORD=$TYPEORM_PASSWORD \
+        -e POSTGRES_INITDB_ARGS=--encoding=UTF-8 \
+        -p 5432:5432 \
+        -v /data/postgres/db-data:/var/lib/postgresql/data \
+        --network xrcloud \
+        postgres:13.11
+
     sleep 5
-else
-    docker run --log-opt max-size=10m --log-opt max-file=3 -d --restart=always -p 5432:5432 --name db -d -e POSTGRES_PASSWORD="$DB_PASSWORD" postgres:11-bullseye || true
 fi
 
 container_name="postgres"
@@ -31,8 +38,8 @@ while ! docker logs "$container_name" 2>&1 | grep -q "$log_message"; do
     sleep 1
 done
 
-docker exec postgres psql -U $TYPEORM_USERNAME -c "CREATE DATABASE ret_dev;"
-docker exec postgres psql -U $TYPEORM_USERNAME -d ret_dev -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
-docker exec postgres psql -U $TYPEORM_USERNAME -d ret_dev -c "ALTER USER $DB_USER WITH SUPERUSER;"
-docker exec postgres psql -U $TYPEORM_USERNAME -d ret_dev -c "GRANT ALL PRIVILEGES ON DATABASE ret_dev TO $DB_USER;"
-docker exec postgres psql -U $TYPEORM_USERNAME -d ret_dev -c "GRANT CREATE ON DATABASE ret_dev TO $DB_USER;"
+docker exec postgres psql -U $TYPEORM_USERNAME -c "CREATE DATABASE $TYPEORM_DATABASE;"
+docker exec postgres psql -U $TYPEORM_USERNAME -d $TYPEORM_DATABASE -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';"
+docker exec postgres psql -U $TYPEORM_USERNAME -d $TYPEORM_DATABASE -c "ALTER USER $DB_USER WITH SUPERUSER;"
+docker exec postgres psql -U $TYPEORM_USERNAME -d $TYPEORM_DATABASE -c "GRANT ALL PRIVILEGES ON DATABASE $TYPEORM_DATABASE TO $DB_USER;"
+docker exec postgres psql -U $TYPEORM_USERNAME -d $TYPEORM_DATABASE -c "GRANT CREATE ON DATABASE $TYPEORM_DATABASE TO $DB_USER;"
