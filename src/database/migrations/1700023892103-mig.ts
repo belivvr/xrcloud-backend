70478d7366464d0bb0e59704d20033ee91e79bc0
jdm <zizi27177@naver.com>
2023-11-16 04:45:54 +0000

    .

--- src/database/migrations/1700023892103-mig.ts
+++ src/database/migrations/1700023892103-mig.ts
@@ -5,7 +5,7 @@ export class Mig1700023892103 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
-            `CREATE TABLE "main"."users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "version" integer NOT NULL, "projectId" character varying NOT NULL, "infraUserId" character varying NOT NULL, "reticulumId" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`
+            `CREATE TABLE "main"."users" ("id" uuid NOT NULL DEFAULT main.uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "version" integer NOT NULL, "projectId" character varying NOT NULL, "infraUserId" character varying NOT NULL, "reticulumId" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`
         )
     }
 
