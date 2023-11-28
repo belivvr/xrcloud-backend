78df91fb7171b08173a9772af8201e5f85c32686
jdm <zizi27177@naver.com>
2023-11-28 08:40:15 +0000

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,55 @@
 #!/bin/bash
 
-i=${1:-latest}
+#
+HOME_DIR="/home/jdm/workspace"
+PROJECT_DIR=$(dirname $(dirname $(realpath $0)))
+PROJECT_NAME=$(basename "$PROJECT_DIR")
+ENV_FILE="$PROJECT_DIR/.env"
+LOGS_DIR="$HOME_DIR/logs/$PROJECT_NAME"
 
 #
-cd ~/workspace/xrcloud-backend
+get_latest_tag() {
+    local TAG=$(docker images | grep "$PROJECT_NAME" | awk '{print $2}' | grep -E '^[0-9]+$' | sort -r | head -n 1)
+    echo ${TAG:-0}
+}
+
+LATEST_TAG=$(get_latest_tag)
+NEW_TAG=$((LATEST_TAG + 1))
+
+# check env
+if [[ ! -f "$ENV_FILE" ]]; then
+    echo "Environment file not found: $ENV_FILE"
+    exit 1
+fi
+
+mkdir -p "$LOGS_DIR"
+
+# move to project dir
+cd "$PROJECT_DIR" || exit 1
+echo "Working directory changed to $(pwd)"
+
+# pull & build
 git pull origin main || exit 1
 
-#
-docker stop backend || true
-docker rm backend || true
+docker build -t "$PROJECT_NAME:$NEW_TAG" . || exit 1
 
-#
-docker build -t "backend-$i" . || exit 1
+# rm prev container
+docker stop "$PROJECT_NAME" || true
+docker rm "$PROJECT_NAME" || true
 
-#
 docker network create xrcloud || true
 
-#
+# run next container
 docker run --restart always -d \
     -e NODE_ENV=production \
-    --name backend \
+    --name $PROJECT_NAME \
     --network xrcloud \
     --log-opt max-size=10m \
     --log-opt max-file=3 \
-    -v /home/jdm/workspace/xrcloud-backend/.env:/app/.env \
-    -v /home/jdm/workspace/logs/backend:/app/logs \
-    "backend-$i" || exit 1
+    -v "$ENV_FILE":/app/.env \
+    -v "$LOGS_DIR":/app/logs \
+    "$PROJECT_NAME:$NEW_TAG" || exit 1
 
-#
-docker system prune -f || exit 1
+docker system prune -f || true
 
-#
-echo "Docker container backend-$i is up and running."
+echo "Docker container $PROJECT_NAME is up and running."
