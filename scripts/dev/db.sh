98c39a162087bf6d8e129393d84994761b000098
zizi2717 <zizi27177@naver.com>
2024-01-26 16:53:35 +0900

    Refactor database script to improve readability and add port mapping

--- scripts/dev/db.sh
+++ scripts/dev/db.sh
@@ -5,16 +5,15 @@ set -e
 
 docker rm -f $TYPEORM_HOST
 
-docker run --restart always --name $TYPEORM_HOST -d \
-        --log-opt max-size=10m \
-        --log-opt max-file=3 \
-        -e POSTGRES_DB=$TYPEORM_DATABASE \
-        -e POSTGRES_USER=$TYPEORM_USERNAME \
-        -e POSTGRES_PASSWORD=$TYPEORM_PASSWORD \
-        -e POSTGRES_INITDB_ARGS=--encoding=UTF-8 \
-        -v /data/postgres/xrcloud:/var/lib/postgresql/data \
-        --network xrcloud \
-        postgres:13.11
+docker run --restart=always --name $TYPEORM_HOST -d --log-opt max-size=10m --log-opt max-file=3 \
+    --network xrcloud \
+    -p 5432:5432 \
+    -e POSTGRES_DB=$TYPEORM_DATABASE \
+    -e POSTGRES_USER=$TYPEORM_USERNAME \
+    -e POSTGRES_PASSWORD=$TYPEORM_PASSWORD \
+    -e POSTGRES_INITDB_ARGS=--encoding=UTF-8 \
+    -v /data/postgres/xrcloud:/var/lib/postgresql/data \
+    postgres:13.11
 
 LOG_MSG="listening on IPv4 address \"0.0.0.0\", port 5432"
 
